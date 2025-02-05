283870dd4fb3e31b227312fc391c514c4e7c46eb
AksaJudith/Winter <aksa.stranden@centria.fi>
2025-02-05 12:09:27 +0200

    nodeserver

---
+++ NodeAndExpress/serverNode.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+    if (req.method === "GET" && req.url === "/") {
+        //REad HTML file
+        const filePath = path.join(__dirname, "combined.html");
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { "Content-Type": "text/plain"});
+                res.end("An error occured on the server.");
+            } else {
+                res.writeHead(200, { "Content-Type": "text/html"});
+                res.end(data);
+            }
+        });
+    } else {
+        res.writeHead(404, { "Content-Type": "text/plain"});
+        res.end("Page not found");
+    }
+});
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://MACHINE-IP-ADDRESS:${PORT}`);
+});
